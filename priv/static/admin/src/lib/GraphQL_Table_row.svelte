87dcf51c59c54f247189540191886bf7798a483e
Roy Davies <roy.c.davies@ieee.org>
2023-06-17 15:43:33 +1200

    WIP Admin WebApp

--- priv/static/admin/src/lib/GraphQL_Table_row.svelte
+++ priv/static/admin/src/lib/GraphQL_Table_row.svelte
@@ -6,18 +6,21 @@
   Contact: roy.c.davies@ieee.org
 ------------------------------------------------------------------------------------------------------->
 <script lang="ts">
-    import { reload, Table, Table_Col, Table_Row, Table_Body, Table_Foot, Table_Head, Checkbox, Image, Label, Popup, Calendar, Input, Dropdown, Link, Menu, Item, Button, Icon, Text, Buttons } from "svelte-fomantic-ui";
-    import { getKeys, getFormat, totalWidth, proportionalWidth } from "./Graphql.svelte";
+    import { reload, Table_Col, Checkbox, Image, Label, Popup, Link } from "svelte-fomantic-ui";
     import { onMount } from "svelte";
+    import GraphQL_Table from "./GraphQL_Table.svelte";
 
-    export let col: string = "";
+    export let col: string = undefined;
     export let row: any = {};
     export let format: any = {};
     export let horizontal = false;
 
+    console.log({col:col, row:row, format: format, Horiz: horizontal});
+
     onMount(() => {
         reload();
     });
+
     function extract (data:any[], keystring:string) {
         let result = data;
         let keys = keystring.split('.');
@@ -28,48 +31,50 @@
     }
 </script>
 
-{#if horizontal}
+{#if col && col !== ""}
+    {#if horizontal}
+        <Table_Col center aligned>
+            {col}
+        </Table_Col>
+    {/if}
     <Table_Col center aligned>
-        {col}
+        {#if (format[col].input === "text" || format[col].input === "number") && row[col] !== undefined}
+            {format[col].capitalise ? row[col].toUpperCase() : row[col]}
+        {:else if format[col].input === "checkbox" && row[col] !== undefined}
+            <Checkbox checked={row[col]}/>
+        {:else if format[col].input === "datetime" && row[col] !== undefined}
+            <Label ui green>{row[col].toLocaleString('en-US', { timeZone: 'UTC', dateStyle: 'short', timeStyle: 'short' })}</Label>
+        {:else if format[col].input === "image" && row[col] !== undefined}
+            <Link href={row[col]}><Image ui avatar src={row[col]}/>{row[col]}</Link>
+        {:else if format[col].input === "url" && row[col] !== undefined}
+            <Link href={row[col]}>{row[col]}</Link>
+        {:else if format[col].input === "object" && row[col] !== undefined}
+            <GraphQL_Table horizontal definition={format[col].type} data={[row[col]]}/>
+        {:else if format[col].input === "array" && row[col] !== undefined}
+            {#if format[col].hasOwnProperty("labelise")}
+                {#each row[col] as item, i}
+                    <Label ui blue popup={{hoverable: true}}>{extract(item, format[col].labelise)}</Label>
+                    <Popup ui>
+                        <GraphQL_Table definition={format[col].type} data={[item]}/>
+                    </Popup>
+                {/each}
+            {:else}
+                <GraphQL_Table definition={format[col].type} data={[row[col]]}/>
+            {/if}
+        {:else if format[col].input === "dropdown" && row[col] !== undefined}
+            <!-- <Dropdown ui fluid selection selected={data[col].status.toString()} on:change={(item) => {data[col].status=Number(item.detail.value)}}>
+                <Input hidden bind:value={data[col].status}/>
+                <Icon dropdown/>
+                <Text default>Select Status</Text>
+                <Menu fluid>
+                    <Item value="0">student</Item>
+                    <Item value="1">staff</Item>
+                    <Item value="2">admin</Item>
+                </Menu>
+            </Dropdown> -->
+            {"dropdown"}
+        {:else}
+            ---
+        {/if}
     </Table_Col>
 {/if}
-<Table_Col center aligned>
-    {#if (format[col].input === "text" || format[col].input === "number") && row[col] !== undefined}
-        {format[col].capitalise ? row[col].toUpperCase() : row[col]}
-    {:else if format[col].input === "checkbox" && row[col] !== undefined}
-        <Checkbox checked={row[col]}/>
-    {:else if format[col].input === "datetime" && row[col] !== undefined}
-        <Label ui green>{row[col].toLocaleString('en-US', { timeZone: 'UTC', dateStyle: 'short', timeStyle: 'short' })}</Label>
-    {:else if format[col].input === "image" && row[col] !== undefined}
-        <Link href={row[col]}><Image ui avatar src={row[col]}/>{row[col]}</Link>
-    {:else if format[col].input === "url" && row[col] !== undefined}
-        <Link href={row[col]}>{row[col]}</Link>
-    {:else if format[col].input === "object" && row[col] !== undefined}
-        <svelte:self horizontal definition={format[col].type} data={[row[col]]}/>
-    {:else if format[col].input === "array" && row[col] !== undefined}
-        {#if format[col].hasOwnProperty("labelise")}
-            {#each row[col] as item, i}
-                <Label ui blue popup={{hoverable: true}}>{extract(item, format[col].labelise)}</Label>
-                <Popup ui>
-                    <svelte:self definition={format[col].type} data={[item]}/>
-                </Popup>
-            {/each}
-        {:else}
-            <svelte:self definition={format[col].type} data={[row[col]]}/>
-        {/if}
-    {:else if format[col].input === "dropdown" && row[col] !== undefined}
-        <!-- <Dropdown ui fluid selection selected={data[col].status.toString()} on:change={(item) => {data[col].status=Number(item.detail.value)}}>
-            <Input hidden bind:value={data[col].status}/>
-            <Icon dropdown/>
-            <Text default>Select Status</Text>
-            <Menu fluid>
-                <Item value="0">student</Item>
-                <Item value="1">staff</Item>
-                <Item value="2">admin</Item>
-            </Menu>
-        </Dropdown> -->
-        {"dropdown"}
-    {:else}
-        ---
-    {/if}
-</Table_Col>
